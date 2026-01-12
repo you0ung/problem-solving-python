16e4f3c554069eba6325b2be2a96a62dd2fad039
YuHyun Lee <164338358+you0ung@users.noreply.github.com>
2026-01-12 13:46:59 +0900

    Delete intro directory

--- intro/day2/3-2.py
+++
@@ -1,7 +0,0 @@
-def solution(array):
-    array.sort()
-    middle = len(array) // 2
-    
-    return array[middle]
-
-print(solution([9, -1, 0]))
--- intro/day2/3-4.py
+++
@@ -1,9 +0,0 @@
-def solution(n):
-    array = []
-    for i in range(1, n+1):
-        if i % 2 == 1:
-            array.append(i)
-
-    return array
-
-print(solution(15))
